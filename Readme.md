c0fc206927cd78b07baf729193133767233c64cd
Menros <gondras.plouis@gmail.com>
2023-01-06 11:39:55 +0100

    create installer

--- Readme.md
+++ Readme.md
@@ -1,11 +1,19 @@
 # Installation
-
-## Release
-Dowload latest release (zip)  
-Unsip files where you want  
+## Automatic installation
+Download latest [release installer](https://github.com/Menros/SyncRom/releases/latest)  
+Execute and follow the installation instructions  
+## Manual installation
+Dowload latest [release (zip)](https://github.com/Menros/SyncRom/releases/latest)  
+Unzip files where you want  
 Execute SyncRom.exe  
 
-## Manual
+# Manual compilation
+## Requirements
+You must install:  
+- [ps2exe](https://github.com/MScholtes/PS2EXE): allows to compile powershell script to exe file
+- [inno setup](https://jrsoftware.org/isinfo.php): allows to generate installer
+    - You must set the ino setup folder in your PATH
+
 Clone project (or download source code from any release)  
 Execute in powershell : .\compile.ps1 -prod  
-Get the zip in "out" folder, then follow Release instructions
+Get the files in "out" folder, then follow Installation instructions
--- compile.ps1
+++ compile.ps1
@@ -1,3 +1,5 @@
+$version="1.0.1"
+
 $env="dev";
 if($args.contains('-dev')) {$env="dev"}
 if($args.contains('-prod')) {$env="prod"}
@@ -5,15 +7,12 @@ if($args.contains('-prod')) {$env="prod"}
 
 # Create and empty build and out folders
 $path = ".\build"
-if (-Not (Test-Path $path)) {
-    New-Item -Path $path -ItemType Directory | Out-Null
-}
-Get-ChildItem -Path $path -Include * -Recurse | ForEach-Object { $_.Delete()}
+Remove-Item -Recurse -Force $path
+New-Item -Path $path -ItemType Directory | Out-Null
+
 $path = ".\out"
-if (-Not (Test-Path $path)) {
-    New-Item -Path $path -ItemType Directory | Out-Null
-}
-Get-ChildItem -Path $path -Include * -Recurse | ForEach-Object { $_.Delete()}
+Remove-Item -Recurse -Force $path
+New-Item -Path $path -ItemType Directory | Out-Null
 
 # copy app icon to build folder
 Copy-Item .\src\icon.ico -Destination .\build\icon.ico -Force
@@ -24,6 +23,15 @@ if ($env -eq "prod") {
 else {
     Copy-Item .\src\config.json -Destination .\build\config.json -Force
 }
+
+# set version
+$file = (Get-Content ".\build\config.json") -as [Collections.ArrayList]
+$file[1] = '    "version":  "'+$version+'",'
+$file | Set-Content .\build\config.json
+
+$file = (Get-Content ".\syncRomInstaller.iss") -as [Collections.ArrayList]
+$file[4] = '#define MyAppVersion "'+$version+'"'
+$file | Set-Content .\syncRomInstaller.iss
 
 
 # get compilation files
@@ -39,17 +47,23 @@ for ($i = $XAML.Count-1; $i -gt -1; $i--) {
 $codeFile.Insert(4, '[xml]$XAML = @"')
 $codeFile | Set-Content .\build\SyncRom_compiled.ps1
 
+# create compilation folder
+$outPath = ".\out\SyncRom-"+$version
+New-Item -Path $outPath -ItemType Directory | Out-Null
+
 # Compile to exe
 Import-Module ps2exe
-ps2exe -inputFile .\build\SyncRom_compiled.ps1 -outputFile .\out\SyncRom.exe -noConsole -title SyncRom -iconFile .\build\icon.ico -version $config.version
+ps2exe -inputFile .\build\SyncRom_compiled.ps1 -outputFile $outPath\SyncRom.exe -noConsole -title SyncRom -iconFile .\build\icon.ico -version $config.version
 
 # copy config.json
-Copy-Item .\build\config.json -Destination .\out\config.json -Force
+Copy-Item .\build\config.json -Destination $outPath\config.json -Force
 
 # compress zip
 $compress = @{
-    Path = ".\out\*"
+    Path = $outPath
     CompressionLevel = "Fastest"
-    DestinationPath = ".\out\SyncRom.zip"
+    DestinationPath = ".\out\SyncRom-"+$version+".zip"
 }
 Compress-Archive @compress
+
+iscc /O"out" ".\syncRomInstaller.iss"
